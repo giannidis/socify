74b585a2add9a5e99441cc1a7ebbd0a70b18c195
unknown <giannidis50@hotmail.com>
2015-10-16 14:27:05 +0300

    added age to signups

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'font-awesome-rails'
 
+gem 'mailboxer'
+
 group :development do
   gem 'spring'
   gem 'better_errors'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :password_confirmation]
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :age, :password_confirmation]
     devise_parameter_sanitizer.for(:sign_in) << [:email, :remember_me]
   end
 
