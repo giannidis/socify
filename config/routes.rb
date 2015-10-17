4118ec8854611b5753e718bcb77667108ac4f958
unknown <giannidis50@hotmail.com>
2015-10-17 22:23:11 +0300

    added messages full fuctionallity (without chosen rails gem)

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,16 @@ Rails.application.routes.draw do
   get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
   get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
 
+
+  # conversations
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
